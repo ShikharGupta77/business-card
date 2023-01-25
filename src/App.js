00e67d1715386fc2f28a1d75de9124a58f00fbad
Shikhar Gupta <shikhargupta@Shikhars-iPhone-14.local>
2023-01-24 22:26:50 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import TopInfo from "./TopInfo";
+import ContactInfo from "./ContactInfo";
+import Description from "./Description";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="background">
+                <TopInfo />
+                <ContactInfo />
+                <Description />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/ContactInfo.js
@@ -0,0 +1,8 @@
+function ContactInfo() {
+    return (
+        <div className="contact-info">
+            <h1>Contact Info</h1>
+        </div>
+    );
+}
+export default ContactInfo;
